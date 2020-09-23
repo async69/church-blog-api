f2db59e79373719f5e4f777274aa004994cd38a1
MacBook <pro@MacBooks-MacBook-Pro.local>
2020-09-24 02:03:39 +0300

    Added coverage

--- src/models/User/__tests__/db.unit.spec.ts
+++ src/models/User/__tests__/db.unit.spec.ts
@@ -1,18 +1,22 @@
-// import User from '../index'
+import { UserModel } from '../user.model'
+
+const run = () => "Hello"
 
 describe('Create users', () => {
     it('Should create new users successfully', () => {
-        // const mockUser = {
-        //     firstName: 'one',
-        //     lastName: 'two',
-        //     email: 'email'
-        // }
+        const mockUser = {
+            _id: "some id",
+            firstName: 'one',
+            lastName: 'two',
+            age: 23
+        }
 
-        // const spy = jest.spyOn(User, "create").mockReturnValueOnce(mockUser as any)
-        // User.create(mockUser)
-        // const spyCreatedUser = spy.mock.results[0].value
-        // expect(spy).toHaveBeenCalledTimes(1)
-        // expect(spyCreatedUser.firstName).toEqual(mockUser.firstName)
-        // spy.mockReset()
+        const spy = jest.spyOn(UserModel, "create").mockReturnValueOnce(mockUser as any)
+        UserModel.create(mockUser)
+        const spyCreatedUser = spy.mock.results[0].value
+        expect(spy).toHaveBeenCalledTimes(1)
+        expect(spyCreatedUser.firstName).toEqual(mockUser.firstName)
+        spy.mockReset()
+        // expect(typeof run()).toEqual("string")
     })
 })
